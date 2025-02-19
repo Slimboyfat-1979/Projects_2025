d657a82410ad56ff62ddd2ad3b691c0c61d4cd50
Slimboyfat-1979 <slimboyfat1979@gmail.com>
2025-02-19 22:45:39 +1300

    Added Posts

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,30 @@
 <template>
-   <Navigtion></Navigtion>
-</template>
-
-<script>
-    import Navigtion from './components/Navigtion.vue';
-    export default {
-        components: {
-            Navigtion
-        }
-    }
-</script>
-
-<style>
-
-    * {
-        margin: 0;
-    }
-</style>
+    <Navigtion></Navigtion>
+    <div v-for="post in posts" :key="post.id">
+        <p>{{ post.content }}</p>
+    </div>
+ </template>
+ 
+ <script>
+ import Navigtion from './components/Navigtion.vue';
+ 
+ export default {
+     components: {
+         Navigtion
+     },
+     data() {
+         return {
+             posts: [
+                 { id: 1, content: "This is the first post" },
+                 { id: 2, content: "This is the second post" }
+             ]
+         }
+     }
+ }
+ </script>
+ 
+ <style>
+     * {
+         margin: 0;
+     }
+ </style>
